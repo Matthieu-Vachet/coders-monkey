1493962a90ed8830af6f8792c5c3a590c8b36700
Matthieu Vachet <Tremplin_7@hotmail.fr>
2024-08-14 22:35:07 +0200

    .

---
+++ src/config/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAS0HXZM40KxT__52oO-JqO8Nb5SXE56-o",
+    authDomain: "codersmonkeys-d4ce2.firebaseapp.com",
+    projectId: "codersmonkeys-d4ce2",
+    storageBucket: "codersmonkeys-d4ce2.appspot.com",
+    messagingSenderId: "1019591323933",
+    appId: "1:1019591323933:web:bcadba2eec31aa2692b1f4",
+};
+
+const app = initializeApp(firebaseConfig);
