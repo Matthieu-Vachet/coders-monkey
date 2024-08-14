6636009e33a47f28c38a4cc7093c963fa9e6378f
Matthieu Vachet <Tremplin_7@hotmail.fr>
2024-08-15 01:16:17 +0200

    Ajout du design-system "typography"

---
+++ .firebase/codersmonkeys-d4ce2/hosting/_next/static/-P62cpENpu4rqN5k_D-B-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-4ffd378f0fd43c2b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
+import { Typography } from "@/ui/design-system/typography/typography";
 import { Seo } from "../ui/components/seo";
 
 export default function Home() {
     return (
         <>
             <Seo title="Coders Monkeys" description="Description..." />
-            <h1>Home Page</h1>
+            <Typography variant="display" component="h1">
+                Coders Monkey
+            </Typography>
+            <Typography theme="primary" variant="body-lg" component="h1">
+                Coders Monkey
+            </Typography>
+            <Typography theme="secondary" variant="lead" component="h1">
+                Coders Monkey
+            </Typography>
+            <Typography variant="body-sm" component="h1">
+                Coders Monkey
+            </Typography>
+            <Typography variant="caption4" component="h1">
+                Coders Monkey
+            </Typography>
         </>
     );
 }
